15d156f4415be3f4125f0f887956282ccc871081
Naji-97 <elbairabennaji@gmail.com>
2023-06-07 13:28:26 +0100

    create owners, species tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -13,3 +13,24 @@ CREATE TABLE animals (
 
 -- Add the species column to the animals table
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+-- Multi Table
+
+-- Create owners table
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+-- Create species table
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- Modify animals table
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
