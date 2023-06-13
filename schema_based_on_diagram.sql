e7cbe9968600e4c71c931f8fcbfa959173d68986
Naji-97 <elbairabennaji@gmail.com>
2023-06-13 19:33:29 +0100

    create invoices and  invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,25 @@ CREATE TABLE treatments (
 	name VARCHAR(100),
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE invoices (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INT,
+	PRIMARY KEY(id),
+	CONSTRAINT fk_med_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT,
+	treatment_id INT,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+	CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+);
